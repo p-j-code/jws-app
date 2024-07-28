363361c8a22900e31ec7af0074ce2f9da04c7fd9
p-j-code <135416727+p-j-code@users.noreply.github.com>
2024-07-28 18:23:02 +0530

    Added categorylist

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,44 +1,13 @@
-import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
-import {getAllProductsRequest} from '../../store/actions/productActions';
-import ProductGroup from './components/ProductGroup';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import theme from '../../theme';
+import CategoryList from '../Category/CategoryList';
 
 const HomeScreen = () => {
-  const dispatch = useDispatch();
-  const {products, loading, error} = useSelector(state => state.product);
-
-  useEffect(() => {
-    dispatch(getAllProductsRequest({groupByParentCategories: true}));
-  }, [dispatch]);
-
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <Text style={styles.loadingText}>Loading...</Text>
-      </View>
-    );
-  }
-
-  if (error) {
-    return (
-      <View style={styles.errorContainer}>
-        <Text style={styles.errorText}>Error: {error}</Text>
-      </View>
-    );
-  }
-
   return (
-    <ScrollView style={styles.container}>
-      {Object.keys(products).map(key => (
-        <ProductGroup
-          key={key}
-          parentCategories={products[key].parentCategories}
-          products={products[key].products}
-        />
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <CategoryList />
+    </View>
   );
 };
 
@@ -46,25 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background.default,
-    padding: theme.spacing.medium,
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  loadingText: {
-    ...theme.typography.h2,
-    color: theme.colors.text.primary,
-  },
-  errorContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  errorText: {
-    ...theme.typography.h2,
-    color: theme.colors.status.error,
   },
 });
 
